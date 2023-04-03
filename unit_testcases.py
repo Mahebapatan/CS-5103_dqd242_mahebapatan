66263b2c298ae84327e70f227767fb39c85591bf
Mahebapatan <66388016+Mahebapatan@users.noreply.github.com>
2023-04-03 01:28:33 -0500

    Add files via upload

---
+++ unit_testcases.py
@@ -0,0 +1,27 @@
+from word_count import get_word_freq
+   # The code to test
+import unittest   # The test framework
+text = open("sample1.txt", "r")
+input_string = text.read()
+print(input_string)
+        
+
+class Test_wordcount(unittest.TestCase): 
+       pass      
+       #def test_one_word(self):
+       #    s = "hello"
+       #    self.assertEqual(get_word_freq(s), get_word_freq(s))
+           
+       def test_emptystring(self):
+           text = open("sample1.txt", "r")
+           s = text.read()
+           self.assertEqual(get_word_freq(s), get_word_freq(s))
+           
+       #def test_string(self):
+           #s = "hello"
+        #   self.assertEqual(get_word_freq(s), get_word_freq(s))
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
